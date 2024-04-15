47cf07e766eeb1123710edd17b79294c84d38ea5
Subhadip023 <subhadip240420@gmail.com>
2024-04-15 09:38:04 +0530

    user registration done after a log try !!!

--- routes/joinUsRoute.js
+++ routes/joinUsRoute.js
@@ -1,19 +1,32 @@
 import express from "express";
 import User from "../models/usersModel.js";
-import { convert_to_hash_s10, transporter, generateOTP } from "../userdefineFuntion.js";
+import {
+  convert_to_hash_s10,
+  transporter,
+  generateOTP,
+} from "../userdefineFuntion.js";
 
 const router = express.Router();
 
-const genOtp = generateOTP(4); // Use const instead of let
+const genOtp = generateOTP(4);
+let userdata = {
+  name: null,
+  password: null,
+  email: null,
+  phoneno: null,
+};
+let count = 0;
 
 router.get("/", (req, res) => {
-  res.render("joinUs.ejs", { message: null, error_message: null });
+  const { message } = req.query; // Extract the message from the query parameters
+
+  res.render("joinUs.ejs", { message: message, error_message: null });
 });
 
 router.post("/", async (req, res) => {
   try {
     const { username, password, confirm_password, email, phone } = req.body;
-    req.session.userdata=req.body;
+    req.session.userdata = req.body;
 
     if (password !== confirm_password) {
       return res.render("joinUs.ejs", {
@@ -32,12 +45,13 @@ router.post("/", async (req, res) => {
         error_message: "Email or Phone number already exists",
       });
     }
-
+    const new_password = await convert_to_hash_s10(password);
+    userdata = { name:username, password: new_password, email, phoneno: phone };
     // Define email content
     const mailOptions = {
-      from: 'gyaanhub8@gmail.com',
-      to: 'subhadip240420@gmail.com',
-      subject: 'OTP verification | SFI Aliah ',
+      from: "gyaanhub8@gmail.com",
+      to: "subhadip240420@gmail.com",
+      subject: "OTP verification | SFI Aliah ",
       text: `Hello,
 
       Thank you for using SFI Aliah. Please use the following OTP to verify your account:
@@ -55,37 +69,46 @@ router.post("/", async (req, res) => {
       if (error) {
         return console.log(error);
       }
-      res.redirect('/joinUs/verify');
+      res.redirect("/joinUs/verify");
     });
-    
   } catch (error) {
     console.log(error);
   }
 });
 
-router.post('/verify', async (req, res) => {
-  console.log(req.session.userdata)
-  
-  if (otp == genOtp) { // Compare OTP
-    const new_password = await convert_to_hash_s10(password);
-    const newUser = await User.create({
-      name: username,
-      email,
-      password: new_password,
-      phoneno: phone,
-    });
-    if (!newUser) {
-      console.log("Error adding user");
+router.post("/verify", async (req, res) => {
+  // console.log(req.session.userdata)
+  try {
+    count++;
+    const { otp } = req.body;
+    console.log(req.body);
+    console.log(userdata);
+    if (otp == genOtp) {
+      // Compare OTP
+      console.log("otp matched", "and count " + count);
+      //   const new_password = await convert_to_hash_s10(password);
+        const newUser = await User.create(userdata);
+        if (!newUser) {
+          console.log("Error adding user");
+        }
+        const message = `Welcome to the COMRED's World ${userdata.name}`;
+        res.redirect(`/joinUs?message=${encodeURIComponent(message)}`);
+      } else {
+      if (count === 5) {
+        res.redirect('/joinUs')
+      } else {
+        res.render("verify_otp.ejs", {
+          error_message: "OTP does not match " + (5 - count) + " try left",
+        });
+      }
     }
-    const message = `Welcome to the COMRED's World ${username}`;
-    res.render("joinUs.ejs", { message: message, error_message: null });
-  } else {
-    res.render('verify_otp.ejs', { error_message: 'OTP does not match' });
+  } catch (error) {
+    console.log(error);
   }
 });
 
-router.get('/verify', (req, res) => {
-  res.render('verify_otp.ejs', { error_message: null });
+router.get("/verify", (req, res) => {
+  count = 0;
+  res.render("verify_otp.ejs", { error_message: null });
 });
-
 export default router;
