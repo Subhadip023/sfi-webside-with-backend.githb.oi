8f8082b4f199194e1dd01de12d1ad9ee3e6a34ad
Subhadip023 <subhadip240420@gmail.com>
2024-07-12 20:16:54 +0530

    ajdusstmet 2.0

--- public/js/index.js
+++ public/js/index.js
@@ -4,11 +4,57 @@ const navbar = document.querySelector('.navbar');
 const dropdowenSvg = document.querySelector('.dropdowen ul li');
 // const hn=document.querySelector('.hn');
 const notification_home = document.querySelector('.notification_home');
-const elements = document.querySelectorAll('.kalr_quote');
-const quoteElements = document.querySelectorAll('.kalr_quote');
-const imgElements = document.querySelectorAll('.quotes img');
 
 
+
+// functions 
+
+const showLoader=()=>{
+  document.querySelector('.loader').style.display = 'flex';
+    document.querySelector('.container').style.opacity = '0';
+}
+
+// show notification function for home page
+function showNotification(Notification_title,id) {
+  let hn = document.createElement('div');
+  hn.classList.add('hn');
+  hn.innerHTML = ` 
+
+  <img src="/images/sfi-aliah-log.jpg" alt="sfi logo">
+
+${Notification_title}
+<a href="/notification/Details/${id}"><svg
+    fill="#000000"
+    height="800px"
+    width="800px"
+    version="1.1"
+    id="Layer_1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    viewBox="0 0 330 330"
+    xml:space="preserve"
+  >
+    <path
+      id="XMLID_222_"
+      d="M250.606,154.389l-150-149.996c-5.857-5.858-15.355-5.858-21.213,0.001
+  c-5.857,5.858-5.857,15.355,0.001,21.213l139.393,139.39L79.393,304.394c-5.857,5.858-5.857,15.355,0.001,21.213
+  C82.322,328.536,86.161,330,90,330s7.678-1.464,10.607-4.394l149.999-150.004c2.814-2.813,4.394-6.628,4.394-10.606
+  C255,161.018,253.42,157.202,250.606,154.389z"
+    />
+  </svg></a>`
+  notification_home.appendChild(hn);
+  setTimeout(() => {
+    hn.classList.add('remove'); // Add the 'remove' class to trigger the fade-out and shrink animation
+    setTimeout(() => {
+      hn.remove(); // Remove the element after the animation completes
+    }, 500); // Adjust the timeout to match the duration of the animation
+  }, 5000);
+
+}
+
+
+
+// for nav bar in mobile mode 
 dropdrowen.addEventListener('click', function () {
   navbar.style.display = navbar.style.display === "none" ? "block" : "none";
   if (navbar.style.display == "block") {
@@ -58,20 +104,7 @@ dropdrowen.addEventListener('click', function () {
 });
 window.onload = function () {
 
-  document.querySelector('.banner img').style.opacity = '1';
-// Apply styles to each img element
-imgElements.forEach(element => {
-  element.style.opacity = '1';
-  element.style.boxShadow = '5px 10px 28px red';
-});
-quoteElements.forEach(element => {
-  element.style.opacity = '1';
-});
-
-// Iterate over each element and set their opacity to 1
-elements.forEach(element => {
-  element.style.opacity = '1';
-});
+showLoader();
 
   if (notificationCount != 0) {
     let i = 0;
@@ -87,45 +120,26 @@ elements.forEach(element => {
 
 };
 
-{/* <svg class='cross' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="100" height="100">
-  <line x1="10" y1="10" x2="90" y2="90" stroke="red" stroke-width="10"/>
-  <line x1="90" y1="10" x2="10" y2="90" stroke="red" stroke-width="10"/>
-</svg> */}
 
-function showNotification(Notification_title,id) {
-  let hn = document.createElement('div');
-  hn.classList.add('hn');
-  hn.innerHTML = ` 
 
-  <img src="/images/sfi-aliah-log.jpg" alt="sfi logo">
 
-${Notification_title}
-<a href="/notification/Details/${id}"><svg
-    fill="#000000"
-    height="800px"
-    width="800px"
-    version="1.1"
-    id="Layer_1"
-    xmlns="http://www.w3.org/2000/svg"
-    xmlns:xlink="http://www.w3.org/1999/xlink"
-    viewBox="0 0 330 330"
-    xml:space="preserve"
-  >
-    <path
-      id="XMLID_222_"
-      d="M250.606,154.389l-150-149.996c-5.857-5.858-15.355-5.858-21.213,0.001
-  c-5.857,5.858-5.857,15.355,0.001,21.213l139.393,139.39L79.393,304.394c-5.857,5.858-5.857,15.355,0.001,21.213
-  C82.322,328.536,86.161,330,90,330s7.678-1.464,10.607-4.394l149.999-150.004c2.814-2.813,4.394-6.628,4.394-10.606
-  C255,161.018,253.42,157.202,250.606,154.389z"
-    />
-  </svg></a>`
-  notification_home.appendChild(hn);
-  setTimeout(() => {
-    hn.classList.add('remove'); // Add the 'remove' class to trigger the fade-out and shrink animation
-    setTimeout(() => {
-      hn.remove(); // Remove the element after the animation completes
-    }, 500); // Adjust the timeout to match the duration of the animation
-  }, 5000);
 
-}
 
+
+// For Footer date show
+ // Get the current date
+ let currentDate = new Date();
+
+ // Array of month names
+ let monthNames = [
+   "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+ ];
+
+ // Get the month and year
+ let currentMonthIndex = currentDate.getMonth();
+ let currentMonthName = monthNames[currentMonthIndex];
+ let currentYear = currentDate.getFullYear();
+
+ // Display the month and year in the HTML
+ let copyrightElement = document.getElementById("copyright");
+ copyrightElement.innerHTML = "Copyright © Sfi Aliah Local Committee " + currentMonthName + "-" + currentYear;
